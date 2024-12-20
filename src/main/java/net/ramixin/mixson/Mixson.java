bb0b07dd9446444ff510a68583e0b64232b94bce
Ram <104311709+RamGemes@users.noreply.github.com>
2024-12-20 09:10:57 -0500

    V:0.0.2 - Added modifying resource files in data folder - Fixed Mixson event holder only being able to hold one event per priority value - Fixed jank method of deserializing the JSON file

--- src/main/java/net/ramixin/mixson/Mixson.java
+++ src/main/java/net/ramixin/mixson/Mixson.java
@@ -1,59 +1,47 @@
 package net.ramixin.mixson;
 
-import com.google.gson.Gson;
+
 import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
 import net.minecraft.resource.Resource;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.JsonHelper;
-import org.jetbrains.annotations.NotNull;
 
 import java.io.ByteArrayInputStream;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.TreeSet;
+import java.util.*;
 
 public class Mixson {
 
-     private static final TreeSet<AssociatedMixsonEvent> events = new TreeSet<>();
+    private static final TreeMap<Integer, Set<AssociatedMixsonEvent>> events = new TreeMap<>();
 
 
     public static void registerModificationEvent(Identifier resourceId, Identifier eventId, final MixsonEvent event) {
-        registerModificationEvent(1000, resourceId.withSuffixedPath(".json"), eventId, event);
+        registerModificationEvent(1000, resourceId, eventId, event);
     }
 
     public static void registerModificationEvent(int priority, Identifier resourceId, Identifier eventId, final MixsonEvent event) {
-        events.add(new AssociatedMixsonEvent(resourceId, eventId, event, priority));
+        Set<AssociatedMixsonEvent> eventSet;
+        if(events.get(priority) == null) eventSet = new HashSet<>();
+        else eventSet = events.get(priority);
+        eventSet.add(new AssociatedMixsonEvent(resourceId.withSuffixedPath(".json"), eventId, event));
+        events.put(priority, eventSet);
     }
 
-    public static Iterator<AssociatedMixsonEvent> getEventIterators() {
-        return events.iterator();
-    }
-
-    public record AssociatedMixsonEvent(Identifier resourceId, Identifier eventId, MixsonEvent event, int priority) implements Comparable<AssociatedMixsonEvent> {
-
-        @Override
-        public int compareTo(@NotNull Mixson.AssociatedMixsonEvent o) {
-            return o.priority - priority;
-        }
-    }
+    public record AssociatedMixsonEvent(Identifier resourceId, Identifier eventId, MixsonEvent event) {}
 
     public static Map<Identifier, Resource> runEvents(Map<Identifier, Resource> original) {
         for (Map.Entry<Identifier, Resource> entry : original.entrySet()) {
             Identifier id = entry.getKey();
-            if(!id.getPath().endsWith(".json")) {
-                continue;
-            }
+            if(!id.getPath().endsWith(".json")) continue;
             Resource resource = entry.getValue();
             JsonElement cachedResource = null;
-            Iterator<Mixson.AssociatedMixsonEvent> events = Mixson.getEventIterators();
-            while (events.hasNext()) {
-                Mixson.AssociatedMixsonEvent event = events.next();
-                if(!event.resourceId().equals(id)) continue;
+            for (Set<AssociatedMixsonEvent> eventSet : Mixson.events.sequencedValues()) for (AssociatedMixsonEvent event : eventSet) {
+                if (!event.resourceId().equals(id)) continue;
                 try {
-                    if(cachedResource == null) cachedResource = JsonHelper.deserialize(new Gson(), entry.getValue().getReader(), JsonElement.class);
+                    if (cachedResource == null)
+                        cachedResource = JsonParser.parseReader(entry.getValue().getReader());
                     cachedResource = event.event().run(cachedResource);
                 } catch (Exception e) {
-                    throw new MixsonError(String.format("Failed to modify json file '%s' with event '%s'\n", id, event.eventId())+e);
+                    throw new MixsonError(String.format("Failed to modify json file '%s' with event '%s'\n", id, event.eventId()) + e);
                 }
             }
             if(cachedResource != null) {
